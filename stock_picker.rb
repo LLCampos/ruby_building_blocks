eea3d9f644add74dc8c1f4cbebc484d0b2ee5dba
Luis Campos <luis1212@sapo.pt>
2015-06-03 15:31:31 +0100

    stock_picker completed

---
+++ stock_picker.rb
@@ -0,0 +1,13 @@
+def stock_picker(a)
+	v = [0,0]
+	x = 0 
+	a.each_with_index do |buy, indexb|
+		a[indexb+1 , a.length].each_with_index do |sell, indexs|
+			if (sell - buy) > x
+				v = [indexb,indexs+indexb+1]
+				x = sell - buy
+			end
+		end
+	end
+	p v
+end
