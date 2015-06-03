a925445b3769649d4a0973abce9c5d82e8c24164
Luis Campos <luis1212@sapo.pt>
2015-06-03 10:42:08 +0100

    exercise finished

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,26 @@
-# chr -> turns number into character
-# ord -> turns character into number
+#transforms a letter into its codified form
+def trans (x, n, c)
+	y = c.index(x) + n
+	if y > 25
+		y -= 26
+	end
+	c[y]
+end
 
 def caesar_cipher (string, n=0)
+	if n > 25 then n = n - (n/25)*26 end
+
 	a = string.chars.map do |x|
-		x.ord
-	end
-	a = a.map do |x| 
-		(x - n).chr
+
+		if x =~ /\W/ or x =~/\d/
+			x
+		elsif x =~ /[[:upper:]]/
+			x = trans x, n, ("A".."Z").to_a
+		else
+			x = trans x, n, ("a".."z").to_a 
+		end
+
 	end
 	puts a.join
 end
 
-caesar_cipher "triplo ats", 3
