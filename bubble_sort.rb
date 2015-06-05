f839a437b5ad2b409f3f284a67737376aec166f9
Luis Campos <luis1212@sapo.pt>
2015-06-05 08:33:45 +0100

    buuble sort finished

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,5 @@
 
-def bubble_sort arr
+def bubble_sort arr 
 	#while it is not sorted
 	v = false
 	while v == false
@@ -8,6 +8,24 @@ def bubble_sort arr
 		#loop through the array in pairs
 		while i < arr.length-1
 			if arr[i] > arr[i+1]
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				v = false 
+			end
+			i += 1
+		end
+	end
+	arr
+end
+
+def bubble_sort_by arr
+	#while it is not sorted
+	v = false
+	while v == false
+		v = true
+		i = 0 
+		#loop through the array in pairs
+		while i < arr.length-1
+			if yield(arr[i],arr[i+1]) < 0  
 				arr[i], arr[i+1] = arr[i+1], arr[i]
 				v = false 
 			end
